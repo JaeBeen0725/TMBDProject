1ba55f1efb696bb0d0dd85ad44e81297368126b5
github-username <dhwoqls1@gmail.com>
2023-08-14 09:17:45 +0900

    후우....

--- TMBDProject/ViewController.swift
+++ TMBDProject/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet var movieCollectionView: UICollectionView!
+    var movieList: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        movieCollectionView.delegate = self
+        movieCollectionView.dataSource = self
+        
+    }
+    
+    func tmdb() {
+        
+        let url = "https://api.themoviedb.org/3/trending/movie/week?api_key=d2afb0ae64fac688e7db47527847933d"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                
+                
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
     }
 
+}
 
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TMDBCollectionViewCell", for: indexPath) as? TMDBCollectionViewCell else { return TMDBCollectionViewCell()}
+        
+        
+        
+        
+    }
 }
 
